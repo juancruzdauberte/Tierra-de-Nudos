3ea9dde2a08f3ba0c7eaa093ca8cf9a35210fd90
Juan Cruz Dauberte <juandauberte@gmail.com>
2025-04-15 20:43:11 -0300

    feat: ProtectedRoutes

--- src/components/config/ProtectedRoutes.tsx
+++ src/components/config/ProtectedRoutes.tsx
@@ -2,9 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export const ProtectedRoutes = () => {
-  const { user, loading } = useAuth();
-  if (loading)
-    return <div className="text-center mt-10">Cargando sesión...</div>;
+  const { user } = useAuth();
 
   if (!user) return <Navigate to="/" />;
 
